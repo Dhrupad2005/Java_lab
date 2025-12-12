beec1a677729a67aa6e20ce5d5767996f579c270
Dhrupad <24155325@kiit.ac.in>
2025-12-12 12:55:23 +0530

    first Updated

---
+++ Java_LABS/src/Day2/Dhrupad.java
@@ -0,0 +1,7 @@
+package Day2;
+
+public class Dhrupad {
+    public static void main(String[] args) {
+        System.out.println("Name-Dhrupad Paitandy \n Roll Number- 24155325\n Section-CSE 33\n Branch-CSE(AI & ML)");
+    }
+}
